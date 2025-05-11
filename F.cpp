7ecff31e3e354be7495dacc287e69cb85e0b38ad
Maruf <mdmarufsheikh707@gmail.com>
2025-05-12 01:21:10 +0600

    Add files via upload

---
+++ F.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double r, pi = 3.14159265358979323846;
+    cin >> r;
+    cout << fixed << setprecision(6) << pi* pow(r,2) <<" "<< 2 * pi * r << endl;
+    return 0;
+}
