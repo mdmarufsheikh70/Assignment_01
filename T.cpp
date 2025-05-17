239b562762aea8a131dcf694503d060b50eb4d07
Maruf <mdmarufsheikh707@gmail.com>
2025-05-17 16:45:40 +0600

    Add files via upload

---
+++ T.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+    optimize();
+    int x, y;
+    cin >> x >> y;
+    cout << ((x - 10 * y) / 20) << endl;
+    return 0;
+}
